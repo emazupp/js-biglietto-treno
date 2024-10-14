40e42d9961b069687168362ff4574207d8a2db75
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-14 14:54:04 +0200

    svolgimento

--- js/main.js
+++ js/main.js
@@ -10,10 +10,27 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 /* SVOLGIMENTO */
 
 /* creo una variabile per i km */
+const userKm = parseInt(
+  prompt("Inserisci il numero di chilometri che vuoi percorrere:")
+);
 /* creo una variabile per l'eta dell'utente */
+const userAge = parseInt(prompt("Inserisci la tua eta':"));
 /* creo una variabile per tenere il totale */
 /* moltiplico i chilometri per 0.21 */
+let totalTicketPrice = userKm * 0.21;
 /* condizione di sconto 20% se l'utente ha meno di 18 anni */
+if (userAge < 18) {
+  let coupon = (totalTicketPrice * 20) / 100;
+  totalTicketPrice = totalTicketPrice - coupon;
+}
+
 /* condizione di sconto 40% se l'utente ha più di 65 anni */
+if (userAge >= 65) {
+  let coupon = (totalTicketPrice * 40) / 100;
+  totalTicketPrice = totalTicketPrice - coupon;
+}
 
 /* OUTPUT */
+alert(
+  "Il prezzo totale del biglietto è: " + totalTicketPrice.toFixed(2) + " €"
+);
